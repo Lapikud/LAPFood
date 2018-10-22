a5ac08b6900d9c1b9d12e4823de23a2d8fdb760c
Rasmus Koit <rasmuskoit@gmail.com>
2018-10-23 01:08:30 +0300

    Added rahvatoit and possibility to query multiple posts.

--- backend/facebook/parser.py
+++ backend/facebook/parser.py
@@ -23,7 +23,7 @@ def strip_tags(html):
 
 class FBParser():
     
-    def get_fb_post(self, sub_url):
+    def get_fb_post(self, sub_url, numberOfPosts=1):
         req = requests.get(FB_URL + sub_url)
         html = BeautifulSoup(
             req.text,
@@ -31,15 +31,21 @@ class FBParser():
             parse_only=SoupStrainer(
                 "div",
                 attrs={"data-ad-preview": "message"}))
-
-        inner_div = html.find("div", class_="text_exposed_root").find_all("p")
+        
+        inner_div = html.find_all("div", class_="text_exposed_root")
         html_text_data = []
         stripped_data = []
         menu_data = []
+        for postNr in range(len(inner_div)):
+            if postNr > numberOfPosts:
+                break
+            else:
+                for data in inner_div[postNr]:
+                    html_text_data = str(data).replace("<span class=\"text_exposed_hide\">...</span><span class=\"text_exposed_show\"><br/>", "").split("<br/>")
+                    for text in html_text_data:
+                        if ("TTÜ" in strip_tags(text) and "menüü" in strip_tags(text)):
+                            stripped_data.append(strip_tags(str(text)))
+                        if "€" in strip_tags(text):
+                            stripped_data.append(strip_tags(str(text)))
 
-        for data in inner_div:
-            html_text_data = str(data).replace("<span class=\"text_exposed_hide\">...</span><span class=\"text_exposed_show\"><br/>", "").split("<br/>")
-            for text in html_text_data:
-                if "€" in strip_tags(text):
-                    stripped_data.append(strip_tags(str(text)))
         return stripped_data
