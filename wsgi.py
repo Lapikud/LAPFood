b86e5f1a878258730f19b88b230e6f36a7a27f78
Rasmus Koit <rasmuskoit@gmail.com>
2018-10-24 14:45:13 +0300

    Another test

--- wsgi.py
+++ wsgi.py
@@ -59,7 +59,7 @@ class AllMenus(Resource):
         rahvatoitmenu = fbparser.get_fb_post(sub_url="/rahvatoitttu/posts/", numberOfPosts=1)
         itdailymenu = dailyparser.get_daily_menu("https://päevapakkumised.ee/tallinn/ttü-it-maja-daily")
         vikorpusdailymenu = dailyparser.get_pdfalt_menu("http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_6_korpus.pdf")
-        # testing out if auto commit works
+        # testing out if auto commit works 2
         return {"Bitstop" : bitstopmenu,
                 "Rahvatoit": rahvatoitmenu,
                 "IT Daily": itdailymenu,
